86bb5b00399aa268a55b71b1d9a9c5bfe69074a3
Sihyeon Seong <shyeon0826@gmail.com>
2016-04-22 23:01:28 +0900

    modified:   artistic_alexnet_inference_tutorial.ipynb 	modified:   artistic_alexnet_train_rmsprop.ipynb 	modified:   artistic_alexnet_train_rmsprop_tutorial.ipynb 	modified:   preprocessing_img.ipynb 	modified:   preprocessing_img_tutorial.ipynb

--- setup_nbserver.py
+++ setup_nbserver.py
@@ -1,35 +1,53 @@
+#!/usr/bin/env python
 import os
 
-home_dir = os.environ['HOME']
-
 if os.path.isdir("$HOME/.jupyter/profile_nbserver")is False:
-    os.system("jupyter profile create nbserver")
+    os.system("ipython profile create nbserver")
 else: 
     os.system("echo profile_nbserver is already exist.")
 
-from notebook.auth import passwd
-pwsha = passwd()
+
+    
+yes = set(['yes','y', 'ye', ''])
+no = set(['no','n'])
+
+print ("Do you want to reset password? (y/n)")
+choice = raw_input().lower()
+if choice in yes:
+    from notebook.auth import passwd
+    pwsha = passwd()
    
+    config_str = """
+    # Server config
+    c = get_config()
+    c.NotebookApp.ip = '*'
+    c.NotebookApp.open_browser = False
+    c.NotebookApp.password = u'{}'
+    # It is a good idea to put it on a known, fixed port
+    c.NotebookApp.port = 8888
+    c.NotebookApp.notebookr_dir = u'/home/biadmin/'
+    """.format(pwsha)
+#elif choice in no:
+#else:
+   #print("Please respond with 'y' or 'n'")
+
 config_str = """
 # Server config
 c = get_config()
 c.NotebookApp.ip = '*'
 c.NotebookApp.open_browser = False
-c.NotebookApp.password = u'{}'
 # It is a good idea to put it on a known, fixed port
 c.NotebookApp.port = 8888
-c.NotebookApp.notebook_dir = u'/'
-""".format(pwsha)
+c.NotebookApp.notebookr_dir = u'/home/biadmin/'
+"""
+
+home_dir = os.environ['HOME']
 
 with open(home_dir+"/.jupyter/profile_nbserver/jupyter_notebook_config.py", "w") as cf:
-    cf.write(config_str)
-        
+	cf.write(config_str)
 
-#os.system("sudo printf \"\nexport PATH=/usr/local/cuda/bin:$PATH\nexport LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH\n\" >> /root/.bashrc")
-#os.system("sudo printf \"\nshell -/bin/bash\n\" >> /root/.screenrc")
 
-# this is usually make some error... do not use this!
-#os.system("screen -dRR -dmS ipython_notebook ipython notebook --profile=nbserver;")
+os.system("screen -dRR -dmS jupyter_notebook jupyter notebook --profile=nbserver;")
 
 # https://www.gnu.org/software/screen/manual/screen.html
 # screen install check
